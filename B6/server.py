db69eb0e593ddab18d92ae0bc7703b83227bc852
Viktor O. Renner <renner_vo@bw-sw.com>
2019-11-25 19:07:57 +0700

    Small fixed

--- B6/server.py
+++ B6/server.py
@@ -3,12 +3,12 @@ from bottle import request
 from bottle import route
 from bottle import run
 
-from B6 import album
+from B6.album import *
 
 
 @route("/albums/<artist>")
 def albums(artist):
-    albums_list = album.find(artist)
+    albums_list = find(artist)
     if not albums_list:
         message = "Альбомы исполнителя {} не найдены".format(artist)
         result = HTTPError(404, message)
@@ -21,22 +21,34 @@ def albums(artist):
 
 @route("/albums", method="POST")
 def add_item():
-    album_data = {
-        "year": request.forms.get("year"),
-        "artist": request.forms.get("artist"),
-        "genre": request.forms.get("genre"),
-        "album": request.forms.get("album"),
-    }
-    if album.find_album(album_data["artist"], album_data["album"]):
-        session = album.connect_db()
-        session.add(album_data)
-        session.commit()
-        result = "Альбом успешно добавлен в БД. ID добавленного альбома {}".format(album_data["id"])
+    try:
+        album = Album(
+            year=request.forms.get("year"),
+            artist=request.forms.get("artist"),
+            genre=request.forms.get("genre"),
+            album=request.forms.get("album")
+        )
+        if not verify_year(album.year):
+            raise ValueError("Указанный год не соответствует формату 'YYYY'")
+        if find_album(album.artist, album.album):
+            session = connect_db()
+            session.add(album)
+            session.commit()
+            result = "Альбом успешно добавлен в БД. ID добавленного альбома {}".format(album.id)
+        else:
+            message = "Запись с альбомом {} у исполнителя {} уже есть в БД".format(album.album,
+                                                                                   album.artist)
+            result = HTTPError(409, message)
+        return result
+    except ValueError as err:
+        return HTTPError(409, err)
+
+
+def verify_year(year):
+    if len(year) == 4 & str(year).isdigit():
+        return True
     else:
-        message = "Запись с альбомом {} у исполнителя {} уже есть в БД".format(album_data["album"],
-                                                                               album_data["artist"])
-        result = HTTPError(409, message)
-    return result
+        return False
 
 
 if __name__ == "__main__":
